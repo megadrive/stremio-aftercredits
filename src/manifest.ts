d56eb88c7c4b7f29f063684f9daf20539bacef7b
andy <a.simeon@gmail.com>
2026-01-07 12:59:47 +1100

    chore: added dev monikers for testing

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,9 +1,10 @@
 import type { Manifest } from "stremio-addon-sdk";
+import { appEnv } from "./appEnv.js";
 
 export const manifest: Manifest = {
-  id: "com.almosteffective.aftercredits",
+  id: `${appEnv.isDev ? "dev." : ""}com.almosteffective.aftercredits`,
   version: "1.0.0",
-  name: "AfterCredits",
+  name: `AfterCredits${appEnv.isDev ? " (dev)" : ""}`,
   description: "Are there mid-credits or after credits scenes?",
   resources: ["stream"],
   types: ["movie"],
